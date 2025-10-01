02b122b3c5658b078c9d90091a35cd6283e445f8
ankitpublicstatic <ankitpublicstatic@gmail.com>
2025-10-01 20:41:32 +0530

    Initial commit

---
+++ src/test/java/com/ankit/qbe/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankit.qbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
